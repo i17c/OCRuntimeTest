af198c6252a576273e41bb366fa98f2ee0931531
lifuqiang <195135955@qq.com>
2015-02-26 10:16:48 +0800

    add category test codes

---
+++ RuntimeTest/NSObject+NotImport.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+NotImport.h
+//  RuntimeTest
+//
+//  Created by jason on 15/2/26.
+//  Copyright (c) 2015年 chenyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (NotImport)
+
+- (void)notImportFoo;
+
+@end
---
+++ RuntimeTest/NSObject+NotImport.m
@@ -0,0 +1,18 @@
+//
+//  NSObject+NotImport.m
+//  RuntimeTest
+//
+//  Created by jason on 15/2/26.
+//  Copyright (c) 2015年 chenyang. All rights reserved.
+//
+
+#import "NSObject+NotImport.h"
+
+@implementation NSObject (NotImport)
+
+- (void)notImportFoo
+{
+    NSLog(@"NSObject notImportFoo");
+}
+
+@end
